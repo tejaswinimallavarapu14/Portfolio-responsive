5255ecc5828258d2279426c9d6e53a72fa576daa
mallavaraputejaswini14 <mallavaraputejaswini14@gmail.com>
2025-06-11 19:37:48 +0530

    first commit

---
+++ tejaswini/src/components/Footer/Footer.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { FaEnvelope,  FaLinkedin, FaGithub, FaPhone , FaYoutube } from "react-icons/fa";
+
+const Footer = () => {
+  // Smooth scroll function
+  const handleScroll = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <footer className="text-white py-8 px-[12vw] md:px-[7vw] lg:px-[20vw]">
+      <div className="container mx-auto text-center">
+        {/* Name / Logo */}
+        <h2 className="text-xl font-semibold text-purple-500">Tejaswini Mallavarapu</h2>
+
+        {/* Navigation Links - Responsive */}
+        <nav className="flex flex-wrap justify-center space-x-4 sm:space-x-6 mt-4">
+          {[
+            { name: "About", id: "about" },
+            { name: "Skills", id: "skills" },
+            { name: "Experience", id: "experience" },
+            { name: "Projects", id: "projects" },
+            { name: "Education", id: "education" },
+          ].map((item, index) => (
+            <button
+              key={index}
+              onClick={() => handleScroll(item.id)}
+              className="hover:text-purple-500 text-sm sm:text-base my-1"
+            >
+              {item.name}
+            </button>
+          ))}
+        </nav>
+
+        {/* Social Media Icons - Responsive */}
+        <div className="flex flex-wrap justify-center space-x-4 mt-6">
+          {[
+            { icon: <FaEnvelope />, link: "mailto:mallavaraputejaswini14@gmail.com" },
+{ icon: <FaLinkedin />, link: "https://www.linkedin.com/in/tejaswini-mallavarapu-0a3496300" },
+{ icon: <FaGithub />, link: "https://github.com/tejaswinimallavarapu14" },
+{ icon: <FaPhone />, link: "tel:+91 9666207271" },
+            
+          ].map((item, index) => (
+            <a
+              key={index}
+              href={item.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-xl hover:text-purple-500 transition-transform transform hover:scale-110"
+            >
+              {item.icon}
+            </a>
+          ))}
+        </div>
+
+       
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ tejaswini/src/constants.js
@@ -0,0 +1,341 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import aws from './assets/tech_logo/aws.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+import excelr from './assets/company_logo/excelr.jpeg';
+import aivariant from './assets/company_logo/aivariant.jpeg';
+
+
+
+// Education Section Logo's
+// import glaLogo from './assets/education_logo/gla_logo.png';
+// import bsaLogo from './assets/education_logo/bsa_logo.png';
+// import vpsLogo from './assets/education_logo/vps_logo.png';
+import university from './assets/education_logo/university.png';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import hotel from './assets/work_logo/hotel.png';
+import ecommerce from './assets/work_logo/ecommerce.png';
+import portfolio from './assets/work_logo/portfolio.png';
+import orphanage from './assets/work_logo/orphanage.png';
+import varadhifarms from './assets/work_logo/varadhifarms.png';
+import Swiggy from './assets/work_logo/Swiggy.png';
+
+import excelr2 from './assets/work_logo/excelr2.png';
+
+
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+//skills logos 
+import linux from './assets/tech_logo/linux.jpeg';
+import gitlab from './assets/tech_logo/gitlab.jpg';
+import windows from './assets/tech_logo/windows.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+     
+      { name: 'Redux', logo: reduxLogo },
+      
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+  
+    
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend & Databases',
+    skills: [
+       { name: 'Java', logo: javaLogo },
+      { name: 'Springboot', logo: springbootLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      
+      
+    ],
+  },
+  {
+    title: 'Cloud   &  OS',
+    skills: [
+     
+      {name :"LINUX" ,logo:linux},
+      {name :"WINDOWS" ,logo:windows},
+      { name: 'AWS', logo: aws },
+      {name :"EC2" ,logo:aws},
+      {name :"S3" ,logo:aws},
+      {name :"IAM" ,logo:aws},
+      {name :"RDS" ,logo:aws},
+      
+
+      
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+     { name: 'Sts', logo: springbootLogo },
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Gitlab', logo: gitlab },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: aivariant,
+      role: "Java Fullstack Developer Intern",
+      company: "AiVariant",
+      date: "Oct 2024 - April 2025",
+      desc: "Developed dynamic and scalable web applications using Java and Spring Boot, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive React.js UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "Tailwind CSS",
+        "MongoDb",
+        "Redux",
+        "Java",
+        "SprinBoot",
+        "MySql",
+       
+      ],
+    },
+    {
+      id: 1,
+      img: excelr,
+      role: "Java Fullstack Developer Trainee",
+      company: "ExcelR",
+      date: "May 2024 - Dec 2024",
+      desc: "Trained in Java and Spring Boot, gaining proficiency in both frontend and backend development. Worked on building responsive React.js UI, implementing RESTful APIs, and optimizing application performance within an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "Tailwind CSS",
+        "MongoDb",
+        "Redux",
+        "Java",
+        "SprinBoot",
+        "MySql",
+       
+      ],
+    },
+    // {
+    //   id: 2,
+    //   img: newtonschoolLogo,
+    //   role: "Frontend Intern",
+    //   company: "Newton School",
+    //   date: "September 2021 - August 2022",
+    //   desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+    //   skills: [
+    //     "HTML",
+    //     "CSS",
+    //     "Javascript",
+    //     "Bootstrap",
+    //     "Figma",
+    //     "Material UI",
+    //   ],
+    // },
+  ];
+  
+  export const education = [
+   {
+  id: 0,
+  img: university, 
+  school: "Palamuru University, Telangana",
+  date: " 2022 -  2024",
+  grade: "7.70 CGPA",
+  desc: "I have completed my Master's degree (MCA) in Computer Applications from Palamuru University, Telangana. During this period, I developed a solid understanding of programming concepts, software development practices, and core computer science subjects. My coursework included topics such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Technologies, and Software Engineering. I actively engaged in technical workshops, seminars, and project work, which significantly enhanced my practical skills and confidence. My academic journey at Palamuru University has played a vital role in building my technical foundation and preparing me for a career in the software industry.",
+  degree: "Master of Computer Applications - MCA",
+}
+    // {
+    //   id: 1,
+    //   img: bsaLogo,
+    //   school: "BSA College, Mathura",
+    //   date: "Sept 2018 - Aug 2021",
+    //   grade: "73.2%",
+    //   desc: "I completed my Bachelor's degree in Computer Science (B.Sc.) from BSA College, Mathura. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at BSA College allowed me to work on projects that applied theoretical concepts to real-world problems.",
+    //   degree: "Bachelor of Science - BSC (Computer Science)",
+    // },
+    // {
+    //   id: 2,
+    //   img: vpsLogo,
+    //   school: "Vatsalya Public School Govardhan, Mathura",
+    //   date: "Apr 2017 - March 2018",
+    //   grade: "78%",
+    //   desc: "I completed my class 12 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+    //   degree: "CBSE(XII) - PCM with Computer Science",
+    // },
+    // {
+    //   id: 3,
+    //   img: vpsLogo,
+    //   school: "Vatsalya Public School Govardhan, Mathura",
+    //   date: "Apr 2015 - March 2016",
+    //   grade: "87.5%",
+    //   desc: "I completed my class 10 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Science with Computer.",
+    //   degree: "CBSE(X), Science with Computer Application",
+    // },
+  ];
+  
+  export const projects = [
+  {
+    id: 0,
+    title: "Hotel Management System",
+    description:
+      "A comprehensive hotel management system developed using React.js and Spring Boot. It includes features like room booking, check-in/check-out tracking, customer management, and payment integration. Designed for a seamless and efficient hospitality experience.",
+    image: hotel,
+    tags: ["HTML", "CSS", "JavaScript", "React JS", "Tailwind CSS", "MongoDb", "Redux", "Java", "SprinBoot", "MySql", "Payment Integration"],
+    github: "https://github.com/tejaswinimallavarapu14/Hotel_Management_Springboot.git",
+    // webapp: "https://githubprofiledetective.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "Ecommerce Web Application",
+    description:
+      "An end-to-end ecommerce platform featuring user authentication, product catalog, shopping cart, order placement, admin panel, and integrated payment gateway. Built using React.js and Spring Boot with MySQL for data storage.",
+    image: ecommerce,
+    tags: ["HTML", "CSS", "JavaScript", "React JS", "Tailwind CSS", "MongoDb", "Redux", "Java", "SprinBoot", "MySql", "Payment Integration"],
+    github: "https://github.com/tejaswinimallavarapu14/Ecommerc-application-springboot.git",
+    // webapp: "https://csprep.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Orpahange Management System",
+    description:
+      "A web-based platform designed to manage orphanage operations including resident data, donations, staff records, and event tracking. Built using React JS, it ensures streamlined communication and efficient resource management.",
+    image: orphanage,
+    tags: ["HTML", "CSS", "JavaScript", "React JS", "Tailwind CSS", "MongoDb", "Redux", "Java", "SprinBoot", "MySql"],
+    github: "https://github.com/tejaswinimallavarapu14/orphanage-managent-system.git",
+    // webapp: "https://movie-recommendation-app-jet.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Swiggy Frotnend Clone",
+    description:
+      "A frontend clone of Swiggy built with React.js, mimicking the real-time UI and layout of the popular food delivery platform. Includes menu listings, search functionality, and responsive design optimized for user experience.",
+    image: Swiggy,
+    tags:  ["HTML", "CSS", "JavaScript", "React JS"],
+    github: "https://github.com/tejaswinimallavarapu14/Swiggy-frontend.git",
+    // webapp: "https://www.npmjs.com/package/cmtk-email-validator",
+  },
+  {
+    id: 4,
+    title: "Varadhi Farms Frontend Clone",
+    description:
+      "A visually appealing frontend clone of Varadhi Farms' website using React.js and JavaScript. Highlights include clean design, responsive layout, and custom sections to represent real-time agricultural offerings.",
+    image: varadhifarms,
+    tags: ["HTML", "CSS", "JavaScript", "React JS"],
+    github: "https://github.com/tejaswinimallavarapu14/varadhi-farms-frontend.git",
+    // webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+  },
+  {
+    id: 5,
+    title: "Portfolio",
+    description:
+      "A personal developer portfolio built using React.js to showcase skills, projects, and contact information. Features smooth animations, responsive layout, and a clean user interface for a professional online presence.",
+    image: portfolio,
+    tags: ["HTML", "CSS", "JavaScript", "React JS"],
+    github: "https://github.com/tejaswinimallavarapu14/My-Portfolio.git",
+    webapp: "https://tejaswini-portfolio-website.netlify.app/",
+  },
+
+
+    // {
+    //   id: 6,
+    //   title: "Coding Master",
+    //   description:
+    //     "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
+    //   image: cmLogo,
+    //   tags: ["HTML",
+    //     "CSS",
+    //     "JavaScript",
+    //     "React JS",
+    //     "Tailwind CSS",
+    //     "MongoDb",
+    //     "Redux",
+    //     "Java",
+    //     "SprinBoot",
+    //     "MySql", "Payment Integration"],
+    //   github: "https://codingmasterweb.in/",
+    //   webapp: "https://codingmasterweb.in/",
+    // },
+    // {
+    //   id: 7,
+    //   title: "Image Search App",
+    //   description:
+    //     "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+    //   image: imagesearchLogo,
+    //   tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+    //   github: "https://github.com/codingmastr/Image-Search-App",
+    //   webapp: "https://imagsearch.netlify.app/",
+    // },
+    // {
+    //   id: 8,
+    //   title: "Image Background Remover",
+    //   description:
+    //     "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+    //   image: removebgLogo,
+    //   tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+    //   github: "https://github.com/codingmastr/Image-Background-Remover",
+    //   webapp: "https://removeyourbg.netlify.app/",
+    // },
+  ];  
